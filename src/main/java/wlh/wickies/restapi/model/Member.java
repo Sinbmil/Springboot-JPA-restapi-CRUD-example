2bba0faa358015fe542b4ad41c24aa5294a3e871
Lim Byeong Chan <qud902@naver.com>
2022-07-19 16:26:36 +0900

    Update Member.java

--- src/main/java/wlh/wickies/restapi/model/Member.java
+++ src/main/java/wlh/wickies/restapi/model/Member.java
@@ -20,6 +20,8 @@ public class Member {
     private String pw;
     private String name;
     private String address;
+
+    @Id
     private String phone;
     @CreationTimestamp
     private Date createdAt;
